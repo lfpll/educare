85603f3bf4a0f0ec23f30405edd157a94e7f7cf5
lfpll <G2l4u8i*6>
2019-03-19 20:23:22 -0300

    some small changes on the api

--- node_docker/app.js
+++ node_docker/app.js
@@ -1,11 +1,9 @@
 const Koa = require('koa')
 const JSONStream = require('streaming-json-stringify');
 const sql = require('sql-query').Query();
-var Router = require('koa-router');
-
-
+const Router = require('koa-router');
 const app = new Koa()
-var router = new Router();
+const router = new Router();
 
 //app.context.db = db();
 
@@ -18,23 +16,24 @@ router.get('/docentes', (ctx, next) => {
   let limit = 100
   const params = ctx.query
 
-  
+  // Fields selected on the query
   if ('fields' in params)
   {
-  		select = params['fields']
-  		querySql = querySql.select(select.split(','))
-
-  		delete params['fields']
+		let select = params['fields']
+		querySql = querySql.select(select.split(','))
+	  delete params['fields']
   }
-  if ('limit' in params)
-  {
-  	limit = params['limit']
 
+  // Changing the limit
+ 	if ('limit' in params)
+  {
+
+  	limit = parseInt(params['limit'])
+  	if (limit > 100) limit =100
   	delete params['limit']
   }
-  // Generate a query for the SQL
- 	querySql = querySql.where(params).limit(limit).build()
-  console.log(querySql)
+    // Generate a query for the SQL
+  querySql = querySql.where(params).limit(limit).build()
 });
 
 app.use(router.routes()).use(router.allowedMethods());
