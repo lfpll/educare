a9f7ca92f792f00d7764154df263756860d4a55d
lfpll <G2l4u8i*6>
2019-03-23 07:56:07 -0300

    functionalizing query generator

--- node_docker/app.js
+++ node_docker/app.js
@@ -5,17 +5,12 @@ const Router = require('koa-router');
 const app = new Koa()
 const router = new Router();
 
-//app.context.db = db();
-
-router.get('/docentes', (ctx, next) => {
-
-	// SQL query creator
+const create_query = function (params,table)
+{
+	
 	let sqlSelect = sql.select();
-	querySql = sqlSelect.from('docentes')
-
-  let limit = 100
-  const params = ctx.query
-
+	let querySql = sqlSelect.from('docentes')
+	let limit = 100
   // Fields selected on the query
   if ('fields' in params)
   {
@@ -31,9 +26,19 @@ router.get('/docentes', (ctx, next) => {
   	limit = parseInt(params['limit'])
   	if (limit > 100) limit =100
   	delete params['limit']
-  }
-    // Generate a query for the SQL
-  querySql = querySql.where(params).limit(limit).build()
+  }	
+  return querySql.where(params).limit(limit).build()
+}
+//app.context.db = db();
+router.get('/docentes', (ctx, next) => {
+
+
+	// SQL query creator
+  const params = ctx.query
+
+  // Generate a query for the SQL
+ 	let thisSql =  create_query(params,table='docentes')
+ 	console.log(thisSql)
 });
 
 app.use(router.routes()).use(router.allowedMethods());
