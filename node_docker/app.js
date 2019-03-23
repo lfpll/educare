8249b8c2514b56dce6c5645e89a8710f3174142b
lfpll <G2l4u8i*6>
2019-03-23 08:00:01 -0300

    adding function query

--- node_docker/app.js
+++ node_docker/app.js
@@ -7,32 +7,33 @@ const router = new Router();
 	
 //app.context.db = db();
 const numSearch = (num_sum,fields)  =>{}
-router.get('/docentes', (ctx, next) => {
 
-	// SQL query creator
+// SQL query creator
 const create_query = function (params,table)
 {
-	let sqlSelect = sql.select();
-	let querySql = sqlSelect.from('docentes')
-	let limit = 100
+  let sqlSelect = sql.select();
+  let querySql = sqlSelect.from('docentes')
+  let limit = 100
   // Fields selected on the query
   if ('fields' in params)
   {
-		let select = params['fields']
-		querySql = querySql.select(select.split(','))
-	  delete params['fields']
+    let select = params['fields']
+    querySql = querySql.select(select.split(','))
+    delete params['fields']
   }
 
   // Changing the limit
- 	if ('limit' in params)
+  if ('limit' in params)
   {
 
-  	limit = parseInt(params['limit'])
-  	if (limit > 100) limit =100
-  	delete params['limit']
-  }	
+    limit = parseInt(params['limit'])
+    if (limit > 100) limit =100
+    delete params['limit']
+  } 
   return querySql.where(params).limit(limit).build()
 }
+
+
 //app.context.db = db();
 router.get('/docentes', (ctx, next) => {
 
